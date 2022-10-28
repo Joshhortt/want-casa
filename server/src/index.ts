1877b88a2cdbb471d03c76f6441598a128f4bc30
Joshhortt <joshhortt@yahoo.com>
2022-10-28 14:29:36 +0100

    edit & test script that is responsible using nodemon

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
-import express from "express"
-
+import express from "express";
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => res.send('hello again'));
+// Testing types
+const one: number = 1;
+const two: number = 2;
+// const three: boolean = false;
+// const four: string = "one";
+// const five: null = null;
+// const six: undefined = undefined;
+// const seven: any = {};
+
+app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
 app.listen(port);
 
 console.log(`[app] : http://localhost:${port}`);
