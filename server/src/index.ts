b8e6f7b2fafb402164af9bc5de14678c9eaf6358
Joshhortt <joshhortt@yahoo.com>
2022-10-30 10:53:41 +0000

    add commas and remove brackets

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import { schema } from './graphql'
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
-const app = express()
-const port = 9000
+const app = express();
+const port = 9000;
 
 async function startApolloServer() { 
     const server = new ApolloServer({ schema })
     await server.start()
     server.applyMiddleware({ app, path: '/api' })}startApolloServer()
-    app.listen(port, () => {
-        console.log(`[app]: listening at http://localhost:${port}`)
-    }
-    )
+    app.listen(port);
+    
+        console.log(`[app]: http://localhost:${port}`)
+    
+    
