fc9ef03c38218aef7d3ee023b94b946911a18131
Joshhortt <joshhortt@yahoo.com>
2022-10-30 16:59:48 +0000

    add Listing GraphQL object-typeDefs & resolvers objects

---
+++ server/src/graphql/resolvers.ts
@@ -0,0 +1,23 @@
+// import { IResolvers } from "apollo-server-express";
+import { IResolvers } from "@graphql-tools/utils";
+import { listings } from "../listings";
+
+export const resolvers: IResolvers = {
+  Query: {
+    listings: () => {
+      return listings;
+    }
+  },
+
+  Mutation: {
+    deleteListing: (_root: undefined, { id }: { id: string }) => {
+      for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+          return listings.splice(i, 1)[0];
+        }
+      }
+
+      throw new Error("failed to deleted listing");
+    }
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { schema } from './graphql';
+import { typeDefs, resolvers } from './graphql';
+
 
 const app = express();
 const port = 9000;
 
 async function startApolloServer() { 
-    const server = new ApolloServer({ schema })
+    const server = new ApolloServer({ typeDefs, resolvers })
     await server.start()
     server.applyMiddleware({ app, path: '/api' })}startApolloServer()
     app.listen(port);
