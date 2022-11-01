01f5ea5b2a1b1a98c607abedf629602ba192b869
Joshhortt <joshhortt@yahoo.com>
2022-11-01 17:23:21 +0000

    add MongoClient constructor to Mongo cluster

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,15 @@
 import { MongoClient } from "mongodb";
 
 const user = "user_1";
-
 const userPassword = "password";
-
 const cluster = "cluster0.a8suro4";
 
-export const connectDatabase = () =>  {};
-
 const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
+
+export const connectDatabase = async () => {
+    const client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    const db = client.db("main");
+  };     
