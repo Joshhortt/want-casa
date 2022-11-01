fe03cbc6edfe8a08b78e5a8b4835580772d0c034
Joshhortt <joshhortt@yahoo.com>
2022-11-01 17:03:46 +0000

    add db, set up and declare connection server-mongo

---
+++ server/src/database/index.ts
@@ -0,0 +1,11 @@
+import { MongoClient } from "mongodb";
+
+const user = "user_1";
+
+const userPassword = "password";
+
+const cluster = "cluster0.a8suro4";
+
+export const connectDatabase = () =>  {};
+
+const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
