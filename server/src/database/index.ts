85f5b6f81e43e79e75b00f771746969af3603971
Joshhortt <joshhortt@yahoo.com>
2022-11-07 18:24:38 +0000

    add dotenev variables

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,7 @@
 import { MongoClient } from 'mongodb';
 import { Database } from '../lib/types';
 
-const user = "user_001";
-const userPassword = "";
-const cluster = "";
-
-const uri = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
 
 export const connectDatabase = async (): Promise<Database> => {
     
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
