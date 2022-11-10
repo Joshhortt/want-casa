0caec68141365bad1b0cd061ba770ea077227b96
Joshhortt <joshhortt@yahoo.com>
2022-11-10 13:53:33 +0000

    add to resolvers, processing data from MongoDB.

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,23 +1,35 @@
-// import { IResolvers } from "apollo-server-express";
 import { IResolvers } from "@graphql-tools/utils";
-import { listings } from "../listings";
+import { ObjectId } from "mongodb";
+import { Database, Listing } from "../lib/types";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     }
   },
-
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id)
+      });
+
+      if (!deleteRes.value) {
+        throw new Error("failed to delete listing");
       }
 
-      throw new Error("failed to deleted listing");
+      return deleteRes.value;
     }
+  },
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString()
   }
 };
